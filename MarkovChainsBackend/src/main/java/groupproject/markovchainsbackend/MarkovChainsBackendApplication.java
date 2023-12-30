150927ad3e0851028de23037ae6e4cb1eab6ff04
Borek2002 <damianogdf@gmail.com>
2023-12-30 13:19:44 +0100

    Poprawa konfiguracji środowiska, dodanie springa

---
+++ MarkovChainsBackend/src/main/java/groupproject/markovchainsbackend/MarkovChainsBackendApplication.java
@@ -0,0 +1,13 @@
+package groupproject.markovchainsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarkovChainsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarkovChainsBackendApplication.class, args);
+    }
+
+}
---
+++ MarkovChainsBackend/src/test/java/groupproject/markovchainsbackend/MarkovChainsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package groupproject.markovchainsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarkovChainsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
