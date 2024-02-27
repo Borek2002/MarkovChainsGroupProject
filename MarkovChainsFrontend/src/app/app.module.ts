d6b3ec15f3ad4d04ab4808d78529ca9db507cd90
Borek2002 <damianogdf@gmail.com>
2024-02-27 22:41:57 +0100

    zmiana kolorów, stopka i nawigacja

--- MarkovChainsFrontend/src/app/app.module.ts
+++ MarkovChainsFrontend/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HeaderComponent} from "./component/header/header.component";
+import {MainComponent} from "./component/main/main.component";
+import {NavComponent} from "./component/nav/nav.component";
+import { FooterComponent } from './component/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    NavComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
