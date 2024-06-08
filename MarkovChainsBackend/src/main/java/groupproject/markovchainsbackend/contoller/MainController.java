63f89529dbb58161fc984cea6af0139f5e685609
Kiwing11 <kiwing132@gmail.com>
2024-06-08 22:07:22 +0200

    prod changes

---
+++ MarkovChainsBackend/src/main/java/groupproject/markovchainsbackend/contoller/MainController.java
@@ -0,0 +1,13 @@
+package groupproject.markovchainsbackend.contoller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+    @RequestMapping(value = "/{path:[^\\.]*}")
+    public String redirect() {
+        // Forward to home page so that route is preserved.
+        return "forward:/index.html";
+    }
+}
